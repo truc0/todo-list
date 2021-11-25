e2cfac73ca3ec7455980f0430a8a3c2b5afff8a2
truc0 <22969604+truc0@users.noreply.github.com>
2021-11-25 14:25:07 +0800

    feat: display emergency item for emergency list

--- main.js
+++ main.js
@@ -1,1 +1,61 @@
 import './style.scss'
+
+const todos = [
+  {
+    title: 'Math Homework',
+    description: 'I dont know calculus',
+    due: '',
+    level: 'emergency',
+    isDone: false
+  },
+  {
+    title: 'English Homework',
+    description: 'I dont speak English',
+    due: '',
+    level: 'emergency',
+    isDone: false
+  },
+  {
+    title: 'PE Homework',
+    description: 'I stay at 127.0.0.1',
+    due: '',
+    level: 'emergency',
+    isDone: false
+  },
+]
+
+const emergencyList = document.querySelector('#emergency-todo-list .todo-list')
+
+
+/* update list */
+
+// remove existing list
+while (emergencyList.childElementCount) {
+  emergencyList.removeChild(emergencyList.firstChild)
+}
+
+for (const todo of todos.filter(t => t.level === 'emergency')) {
+  /*
+    <li class="todo-list-item">
+      <h6 class="todo-list-item-title">Physic Homework</h6>
+      <p class="todo-list-item-description">
+        I have too much homework todo.
+      </p>
+    </li>
+  */
+  const item = document.createElement('li')
+  item.classList.add('todo-list-item')
+
+  const itemTitle = document.createElement('h6')
+  itemTitle.classList.add('todo-list-item-title')
+  itemTitle.innerText = todo.title
+
+  const itemDescription = document.createElement('p')
+  itemDescription.classList.add('todo-list-item-description')
+  itemDescription.innerText = todo.description
+
+  item.appendChild(itemTitle)
+  item.appendChild(itemDescription)
+
+  emergencyList.appendChild(item)
+}
