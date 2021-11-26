7deede3411972220e3ded293f23efd0cf9347bbc
truc0 <22969604+truc0@users.noreply.github.com>
2021-11-26 17:11:32 +0800

    feat: create todo (without updating view)

--- main.js
+++ main.js
@@ -74,7 +74,27 @@ showModalBtn.addEventListener('click', evt => {
 const createBtns = document.querySelectorAll('#modal .create')
 for (const btn of createBtns) {
   btn.addEventListener('click', evt => {
+    const form = document.querySelector('#modal form')
+    const data = new FormData(form)
 
+    if (data.get('title') === '') {
+      const formErrors = document.querySelector('#modal .form-errors')
+      formErrors.classList.remove('hidden')
+      return
+    }
+
+    const due = `${data.get('due-date')} ${data.get('due-time')}`
+
+    const todo = {
+      title: data.get('title'),
+      description: data.get('description'),
+      due,
+      isDone: false,
+      level: data.get('level')
+    }
+
+    todos.push(todo)
+    hideModal()
   })
 }
 
