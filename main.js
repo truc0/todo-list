19949b286eb5c9c7b08c0f0d36932206725f1aa1
truc0 <22969604+truc0@users.noreply.github.com>
2021-11-26 18:06:04 +0800

    chore: tidy up main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ import './style.scss'
 import Todo from './js/todo.js'
 import Modal from './js/modal.js'
 
-const { todos, render } = Todo
-const { showModal, hideModal } = Modal
+const { render } = Todo
+const { showModal } = Modal
 
 render()
 
@@ -11,5 +11,3 @@ const showModalBtn = document.querySelector('#create-todo-btn')
 showModalBtn.addEventListener('click', evt => {
   showModal()  
 })
-
-/* Modals */
