d201443c653feebededc17c814c975665a1ea9ef
truc0 <22969604+truc0@users.noreply.github.com>
2021-11-26 18:05:06 +0800

    refactor: move all modal related binding to modal

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,7 @@
+import Todo from './todo.js'
+
+const { todos, render } = Todo
+
 const modal = document.querySelector('#modal')
 const modalContent = document.querySelector('#modal-content')
 
@@ -14,6 +18,42 @@ modal.addEventListener('click', evt => {
     // or close the modal
     hideModal()
 })
+
+const createBtns = document.querySelectorAll('#modal .create')
+for (const btn of createBtns) {
+  btn.addEventListener('click', evt => {
+    const form = document.querySelector('#modal form')
+    const data = new FormData(form)
+
+    if (data.get('title') === '') {
+      const formErrors = document.querySelector('#modal .form-errors')
+      formErrors.classList.remove('hidden')
+      return
+    }
+
+    const due = `${data.get('due-date')} ${data.get('due-time')}`
+
+    const todo = {
+      title: data.get('title'),
+      description: data.get('description'),
+      due,
+      isDone: false,
+      level: data.get('level')
+    }
+
+    todos.push(todo)
+    render()
+
+    hideModal()
+  })
+}
+
+const cancelBtns = document.querySelectorAll('#modal .cancel')
+for (const btn of cancelBtns) {
+  btn.addEventListener('click', evt => {
+    hideModal()
+  })
+}
 
 export default {
     showModal,
--- main.js
+++ main.js
@@ -13,39 +13,3 @@ showModalBtn.addEventListener('click', evt => {
 })
 
 /* Modals */
-
-const createBtns = document.querySelectorAll('#modal .create')
-for (const btn of createBtns) {
-  btn.addEventListener('click', evt => {
-    const form = document.querySelector('#modal form')
-    const data = new FormData(form)
-
-    if (data.get('title') === '') {
-      const formErrors = document.querySelector('#modal .form-errors')
-      formErrors.classList.remove('hidden')
-      return
-    }
-
-    const due = `${data.get('due-date')} ${data.get('due-time')}`
-
-    const todo = {
-      title: data.get('title'),
-      description: data.get('description'),
-      due,
-      isDone: false,
-      level: data.get('level')
-    }
-
-    todos.push(todo)
-    render()
-
-    hideModal()
-  })
-}
-
-const cancelBtns = document.querySelectorAll('#modal .cancel')
-for (const btn of cancelBtns) {
-  btn.addEventListener('click', evt => {
-    hideModal()
-  })
-}
